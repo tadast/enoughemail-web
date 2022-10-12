e2b07b72ae1fd7a28f895af73596cf9f045c4a33
Tadas Tamosauskas <tadastamo@gmail.com>
2022-10-12 21:56:54 +0100

    Add more setup instructions

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,6 @@
 class OrganizationsController < AuthenticatedController
   before_action :set_organization, only: %i[show edit update destroy]
-  skip_before_action :ensure_organisation_set_up, only: [:new, :onboarding, :not_admin]
-
+  skip_before_action :ensure_organisation_set_up
   # GET /organizations/1
   def show
   end
