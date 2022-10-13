e25988e8fd9d0ed0ce798edcf1bdd542b3dd3c8e
Tadas Tamosauskas <tadastamo@gmail.com>
2022-10-13 19:35:49 +0100

    Test scope access

--- app/controllers/organization_credentials_tests_controller.rb
+++ app/controllers/organization_credentials_tests_controller.rb
@@ -4,10 +4,31 @@ class OrganizationCredentialsTestsController < AuthenticatedController
 
   def create
     service = current_user.google_service
+    error_messages = {}
+    users = nil
+    error_messages[:user_access] = check_for_errors do
+      users = service.users
+    end
+
+    if users
+      error_messages[:gmail_basic_settings_access] = check_for_errors do
+        service.list_filters(user_email: users.last.emails.first["address"])
+      end
+    end
+
+    if error_messages.values.flatten.empty?
+      render partial: "success", status: :ok
+    else
+      render partial: "failure", locals: {error_messages: error_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def check_for_errors(&block)
     error_messages = []
     begin
-      users = service.users
-      filters = service.list_filters(user_email: users.last.emails.first["address"])
+      yield
     rescue OpenSSL::PKey::RSAError => e
       error_messages << "The 'private_key' part of Google Credentials is invalid. Ensure newline characters are preserved as encoded '\\n'"
       Rails.logger.warn "Org #{current_user.organization_id} has invalid Google creds: #{e.class} #{e.message}"
@@ -15,14 +36,10 @@ class OrganizationCredentialsTestsController < AuthenticatedController
       error_messages << "Google credentials has to be a valid JSON object"
       Rails.logger.warn "Org #{current_user.organization_id} has invalid Google creds: #{e.class} #{e.message}"
     rescue Signet::AuthorizationError => e
-      error_messages << "Google credentials unauthorized. Have they expired or were removed?"
+      error_messages << "Google credentials unauthorized. Have they expired or were removed? Are all access scopes configured correctly?"
       Rails.logger.warn "Org #{current_user.organization_id} has invalid Google creds: #{e.class} #{e.message}"
     end
 
-    if error_messages.empty? && filters.is_a?(Google::Apis::GmailV1::ListFiltersResponse) && users.is_a?(Array)
-      render partial: "success", status: :ok
-    else
-      render partial: "failure", locals: {error_messages: error_messages}, status: :unprocessable_entity
-    end
+    error_messages
   end
 end
