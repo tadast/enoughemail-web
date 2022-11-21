6ca19eea56a686928c81bd776665489fcaed9575
Tadas Tamosauskas <tadastamo@gmail.com>
2022-11-21 23:25:54 +0000

    Frontend improvements to the templates

--- app/models/filter_rule_template.rb
+++ app/models/filter_rule_template.rb
@@ -2,9 +2,15 @@ class FilterRuleTemplate < ApplicationRecord
   validates :name, presence: true
   validates :email_pattern, presence: true
 
+  def patterns_count
+    email_pattern.to_s.split("OR").size
+  end
+
   # Default Gmail filter criteria string has a max length of 1500, chop
   # the email pattern into syntactically valid chunks within length limits
   def email_pattern_as_array_for_gmail(string_to_split: email_pattern, max_length: 1500, separator: " OR ")
+    return @email_pattern_as_array_for_gmail if @email_pattern_as_array_for_gmail
+
     head = string_to_split[0..(max_length - 1)]
 
     # Partition bang on just before last separator within the limit
@@ -15,7 +21,7 @@ class FilterRuleTemplate < ApplicationRecord
     # Tail will start with a separator, chop it off
     clean_tail = tail.sub(separator, "")
 
-    if tail.size <= max_length
+    @email_pattern_as_array_for_gmail = if tail.size <= max_length
       [clean_head, clean_tail]
     else
       chunked_tail = email_pattern_as_array_for_gmail(string_to_split: clean_tail, max_length: max_length, separator: separator)
