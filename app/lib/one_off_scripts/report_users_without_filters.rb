d96429c0d89d5ebe9ff63b03ce8eb67f6cf8826e
Tadas Tamosauskas <tadastamo@gmail.com>
2022-12-06 14:44:55 +0000

    Add a script to apply filters to newly joined users

--- app/lib/one_off_scripts/report_users_without_filters.rb
+++
@@ -1,8 +0,0 @@
-class OneOffScripts::ReportUsersWithoutFilters
-  def run
-    GmailUser.includes(organization: :filter_rules).all.map do |gmail_user|
-      missing_rules = gmail_user.organization.filter_rules.for_everyone.where.not(id: gmail_user.filter_rules.pluck(:id))
-      [gmail_user.email, missing_rules.map(&:email_pattern)]
-    end.to_h
-  end
-end
---
+++ app/lib/one_off_scripts/users_without_filters.rb
@@ -0,0 +1,21 @@
+class OneOffScripts::UsersWithoutFilters
+  def apply
+    GmailUser.includes(organization: :filter_rules).all.map do |gmail_user|
+      missing_rules = gmail_user.organization.filter_rules.for_everyone.where.not(id: gmail_user.filter_rules.pluck(:id))
+      next unless missing_rules.present?
+
+      service = gmail_user.organization.google_service
+      g_user = service.get_user(user_email: gmail_user.email)
+      missing_rules.map do |filter_rule|
+        filter_rule.apply_to_gmail_and_persist(service: service, g_user: g_user)
+      end
+    end
+  end
+
+  def report
+    GmailUser.includes(organization: :filter_rules).all.map do |gmail_user|
+      missing_rules = gmail_user.organization.filter_rules.for_everyone.where.not(id: gmail_user.filter_rules.pluck(:id))
+      [gmail_user.email, missing_rules.map(&:email_pattern)]
+    end.to_h
+  end
+end
